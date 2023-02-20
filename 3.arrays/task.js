2437a0124e0dd530560320d6c1cea8545ddce435
Xenia Ch <eradonna@yandex.ru>
2023-02-20 17:32:59 +0300

    4 задание готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
-}
+   if (arr1.length !== arr2.length) {
+         return false;
+      } 
+      
+   return arr1.every((number, index) => number === arr2[index]);// решение
+   }
+
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+   let genderedArray = users.filter(user => user.gender === gender);
+   let mappedArray = genderedArray.map(user => user.age);
+   return mappedArray.reduce((acc, age) => acc + age  / mappedArray.length, 0);
+   console.log (mappedArray.reduce((acc, age) => acc + age  / mappedArray.length, 0));
 }
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,49 @@
-function Student(name, gender, age) {
-  
+function Student (name, gender, age) {
+   this.name = name;
+   this.gender = gender;
+   this.age = age;
+   this.marks = []; 
 }
+
+let studA = new Student ('Audrey', 'female', '20');
 
 Student.prototype.setSubject = function (subjectName) {
-  
+   this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+studA.setSubject ("Algebra");
+
+Student.prototype.addMarks = function (...marksToAdd) {
+   if (this.hasOwnProperty('marks') === false) {
+      return;
+   } else {
+      this.marks.push(...marksToAdd);
+   }
 }
+
+studA.addMarks (2, 3, 5, 5, 4);
 
 Student.prototype.getAverage = function () {
-  
+   if (this.hasOwnProperty('marks') === false || this.marks.length === 0) {
+      console.log ('false');
+      return 0;
+      
+   } else {
+      let sumMark = 0;
+      for (let i = 0; i < this.marks.length; i++) {
+         sumMark = sumMark + this.marks[i];
+      }
+      let avgMark = sumMark / this.marks.length;
+      return avgMark;
+   }  
 }
 
+studA.getAverage();
+
 Student.prototype.exclude = function (reason) {
-  
+   this.excluded = reason;
+   delete this.subject;
+   delete this.marks;
 }
+
+studA.exclude('bad grades');
