5d522855d7c5afc43f1a32413dafa0181a9d29e0
Xenia Ch <eradonna@yandex.ru>
2023-03-04 20:01:11 +0300

    задание 6 выполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount (amount) {
+   if (isNaN(Number.parseFloat(amount)) === true) {
+      throw new Error ("Невалидное значение");
+   } 
+   parsedNumber = Number.parseFloat(amount);
+   return parsedNumber;
+}
+
+function validateCount (amount) {
+   try {
+      parseCount (amount);
+      return parsedNumber;
+   } catch (error) {
+      return error;
+   }   
+}
+
+class Triangle {
+   constructor (side1, side2, side3) {
+      this.side1 = side1;
+      this.side2 = side2;
+      this.side3 = side3;
+      if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2) {
+         throw new Error ("Треугольник с такими сторонами не существует");
+      }
+   }
+
+   get perimeter() {
+      return this.side1 + this.side2 + this.side3;
+   }
+
+   get area () {
+      let halfPerimeter = this.perimeter / 2;
+      let areaCounted = (Math.sqrt(halfPerimeter * (halfPerimeter - this.side1) * (halfPerimeter - this.side2) * (halfPerimeter - this.side3))).toFixed(3);
+      return Number(areaCounted);
+   }
+}
+
+function getTriangle (side1, side2, side3) {
+   try {
+      return new Triangle(side1, side2, side3);
+   } catch (error) {
+      return {
+         get area () {
+            return ("Ошибка! Треугольник не существует");
+         },
+
+         get perimeter () {
+            return ("Ошибка! Треугольник не существует")
+         }
+      }      
+   }
+}
+
